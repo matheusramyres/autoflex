51288098a83f1609f8efb5ecc6748f5dd33361e7
Matheus <matheus63360@gmail.com>
2026-02-14 14:33:35 -0300

    Add Menu to SideBar and refactor code

--- autoflex-frontend/src/pages/SideBar.tsx
+++ autoflex-frontend/src/pages/SideBar.tsx
@@ -1,7 +1,11 @@
+import clsx from 'clsx';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MenuHamburger } from '../components/MenuHamburger';
 import { OptionDashboard } from '../components/OptionDashboard';
 
 export const SideBar = () => {
+  const [openSideBar, setOpenSideBar] = useState(false);
   type variant = 'dashboard' | 'products' | 'materials';
   const menuItems = [
     { id: 'dashboard', label: 'Dashboard', to: '/' },
@@ -9,32 +13,48 @@ export const SideBar = () => {
     { id: 'materials', label: 'Materiais', to: '/materials' },
   ];
   return (
-    <aside className="fixed max-w-3xs w-full min-h-screen bg-[#101828] flex flex-col items-center">
-      <header className="mt-7 mb-10 w-full pl-6">
-        <p className="text-xl text-white font-bold">ERP Industrial</p>
-        <p className="text-sm text-[#B0B0B0] font-normal">
-          Gerenciamento de Sistema
-        </p>
-      </header>
-      <nav className="flex flex-col items-center w-full">
-        {menuItems.map((item) => {
-          return (
-            <NavLink
-              key={item.id}
-              to={item.to}
-              className="w-full flex items-center justify-center"
-            >
-              {({ isActive }) => (
-                <OptionDashboard
-                  title={item.label}
-                  variant={item.id as variant}
-                  active={isActive}
-                />
-              )}
-            </NavLink>
-          );
-        })}
-      </nav>
-    </aside>
+    <>
+      <MenuHamburger
+        setOpenSideBar={setOpenSideBar}
+        openSideBar={openSideBar}
+      />
+      <aside
+        className={clsx(
+          'fixed max-w-3xs w-full min-h-screen z-1',
+          'bg-erp-main flex flex-col items-center',
+          '-translate-x-full md:translate-none',
+          'pt-11 md:pt-0',
+          openSideBar ? 'translate-none' : '-translate-x-full',
+          'transition delay-100 duration-300 ease-in-out',
+        )}
+      >
+        <header className="mt-7 mb-10 w-full pl-6">
+          <p className="text-xl text-white font-bold">ERP Industrial</p>
+          <p className="text-sm text-erp-muted font-normal">
+            Gerenciamento de Sistema
+          </p>
+        </header>
+        <nav className="flex flex-col items-center w-full">
+          {menuItems.map((item) => {
+            return (
+              <NavLink
+                key={item.id}
+                to={item.to}
+                className="w-full flex items-center justify-center"
+                onClick={() => setOpenSideBar(false)}
+              >
+                {({ isActive }) => (
+                  <OptionDashboard
+                    title={item.label}
+                    variant={item.id as variant}
+                    active={isActive}
+                  />
+                )}
+              </NavLink>
+            );
+          })}
+        </nav>
+      </aside>
+    </>
   );
 };
